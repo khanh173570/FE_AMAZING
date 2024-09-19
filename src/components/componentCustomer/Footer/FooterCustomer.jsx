a5bf2b91319e1cb6975c0b2d223e09f7ce9556e0
PhuongKhanh <107821229+khanh173570@users.noreply.github.com>
2024-09-20 00:04:14 +0700

    Update FooterCustomer.jsx

---
+++ src/components/componentCustomer/Footer/FooterCustomer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FooterCustomer = () => {
+    return (
+        <footer>
+            <p>Footer khanh</p>
+        </footer>
+    );
+}
+
+export default FooterCustomer;
